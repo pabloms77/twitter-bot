b489e89c510c7a508292b0cbad8296e2f352412a
Iván <ivanmmzz97@gmail.com>
2014-11-22 11:25:01 +0100

    Add sql structure

---
+++ tweets.sql
@@ -0,0 +1,12 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Estructura de tabla para la tabla `tweets`
+--
+
+CREATE TABLE IF NOT EXISTS `tweets` (
+  `id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
